8f7d1aa805541c3f1b8e8018df15371e14c9fefa
Valentine Shevchenko <geniuscasiohl820a@gmail.com>
2018-11-01 10:10:15 +0200

    rest template

--- main.go
+++ main.go
@@ -1,35 +1,64 @@
 package main
 
 import (
-	"io"
+	"encoding/json"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
-	"os"
 )
 
-type app struct{}
+type Person struct {
+	ID        string   `json:"id,omitempty`
+	Firstname string   `json:"firstname,omitempty`
+	Lastname  string   `json:"lastname,omitempty"`
+	Address   *Address `json:"address,omitempty"`
+}
+type Address struct {
+	City  string `json:"city,omitempty"`
+	State string `json:"state,omitempty"`
+}
 
-func (a *app) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	status := http.StatusOK
-	body := `{"status":"ok"}` + "\n"
+var people []Person
 
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(status)
-
-	io.WriteString(w, body)
-	log.Printf("\"%s %s %s\" %d %d\n", r.Method, r.URL.Path, r.Proto, status, len(body))
+func GetPersonEndpoint(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for _, item := range people {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Person{})
+}
+func GetPeopleEndpoint(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(people)
+}
+func CreatePersonEndpoint(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var person Person
+	_ = json.NewDecoder(r.Body).Decode(&person)
+	person.ID = params["id"]
+	people = append(people, person)
+	json.NewEncoder(w).Encode(people)
+}
+func DeletePersonEndpoint(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for index, item := range people {
+		if item.ID == params["id"] {
+			people = append(people[:index], people[index+1:]...)
+			break
+		}
+		json.NewEncoder(w).Encode(people)
+	}
 }
 
 func main() {
-	port, ok := os.LookupEnv("PORT")
-
-	if !ok {
-		port = "8080"
-	}
-
-	log.Printf("Starting server on port %s\n", port)
-
-	if err := http.ListenAndServe(":"+port, &app{}); err != nil {
-		log.Fatalf("Could not start server: %s\n", err.Error())
-	}
+	router := mux.NewRouter()
+	people = append(people, Person{ID: "1", Firstname: "John", Lastname: "Doe", Address: &Address{City: "City X", State: "State X"}})
+	people = append(people, Person{ID: "2", Firstname: "Koko", Lastname: "Doe", Address: &Address{City: "City Z", State: "State Y"}})
+	router.HandleFunc("/people", GetPeopleEndpoint).Methods("GET")
+	router.HandleFunc("/people/{id}", GetPersonEndpoint).Methods("GET")
+	router.HandleFunc("/people/{id}", CreatePersonEndpoint).Methods("POST")
+	router.HandleFunc("/people/{id}", DeletePersonEndpoint).Methods("DELETE")
+	log.Fatal(http.ListenAndServe(":8000", router))
 }
