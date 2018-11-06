191d5276e98dabf2c8e48ab4eb20e97f884b5ee6
Valentine Shevchenko <geniuscasiohl820a@gmail.com>
2018-11-06 17:10:25 +0200

    fix dummy errors

--- main.go
+++ main.go
@@ -8,10 +8,20 @@ import (
 	"net/http"
 	"os"
 	"fmt"
-	"database/sql"
 )
 
-var tags []Person
+type Person struct {
+	ID        string   `json:"id,omitempty`
+	Firstname string   `json:"firstname,omitempty`
+	Lastname  string   `json:"lastname,omitempty"`
+	Address   *Address `json:"address,omitempty"`
+}
+type Address struct {
+	City  string `json:"city,omitempty"`
+	State string `json:"state,omitempty"`
+}
+
+var people []Person
 
 func GetPersonEndpoint(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
@@ -77,12 +87,10 @@ func main() {
 
 	people = append(people, Person{ID: "1", Firstname: "John", Lastname: "Doe", Address: &Address{City: "City X", State: "State X"}})
 	people = append(people, Person{ID: "2", Firstname: "Koko", Lastname: "Doe", Address: &Address{City: "City Z", State: "State Y"}})
-	// People
 	router.HandleFunc("/people", GetPeopleEndpoint).Methods("GET")	
 	router.HandleFunc("/people/{id}", GetPeopleEndpoint).Methods("GET")
 	router.HandleFunc("/people/{id}", CreatePeopleEndpoint).Methods("POST")
 	router.HandleFunc("/people/{id}", DeletePeopleEndpoint).Methods("DELETE")
-	
 	// // Income sources tags
 	// router.HandleFunc("/incomeTag", GetIncomeTagEndpoint).Methods("GET")	
 	// router.HandleFunc("/incomeTag/{id}", GetIncomeTagEndpoint).Methods("GET")
