99f251cc341c89292c1d534ca14cb20061e13297
Valentine (laptop) <geniuscasiohl820a@gmail.com>
2018-11-12 00:18:59 +0200

    add some static files with js

---
+++ static/hello.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $.ajax({
+        url:"https://rest-trello-server.herokuapp.com/income/1"
+    }).then(function(data) {
+    $('.income-id').append(data.id);
+    $('.income-hint').append(data.hint);
+    $('.income-amount').append(data.amount);
+    });
+})
