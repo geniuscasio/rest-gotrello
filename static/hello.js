8c8d33a1ee2b215bcb552fd9a12111cbcce7495d
Valentine Shevchenko <geniuscasiohl820a@gmail.com>
2018-11-12 17:44:00 +0200

    fix html

--- static/hello.js
+++ static/hello.js
@@ -11,13 +11,13 @@ $(document).ready(function() {
             var hint = income.hint;
             var amount = income.amount;
             var html = '<div class="col-sm - 3">'
-                + '<a href="#" class="list-group-item list-group-item-action flex-column align-items-start active">'
+                + '<a href="#" class="list-group-item list-group-item-action flex-column align-items-start">'
                 + '<div class="d-flex w-30 justify-content-between">'
                 + '        <h5 class="mb-1">' + amount + '$</h5>'
                 + '        <small>3 days ago</small>'
                 + '    </div>'
                 + '    <p class="mb-1">' + hint + '</p>'
-                + '    <small>Donec id elit non mi porta.</small>'
+                + '    <small>' + id +'</small>'
                 + '    </a>'
                 + ' </div>'
             $('.row').append(html);
