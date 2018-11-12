2d9db40d5f7a248ef68509220616c59e659c1ed7
Valentine Shevchenko <geniuscasiohl820a@gmail.com>
2018-11-12 17:40:01 +0200

    fix html tamplate for income card

--- static/hello.js
+++ static/hello.js
@@ -11,15 +11,15 @@ $(document).ready(function() {
             var hint = income.hint;
             var amount = income.amount;
             var html = '<div class="col-sm - 3">'
-                + '< a href = "#" class="list-group-item list-group-item-action flex-column align-items-start active" >'
+                + '<a href="#" class="list-group-item list-group-item-action flex-column align-items-start active">'
                 + '<div class="d-flex w-30 justify-content-between">'
                 + '        <h5 class="mb-1">' + amount + '$</h5>'
                 + '        <small>3 days ago</small>'
                 + '    </div>'
                 + '    <p class="mb-1">' + hint + '</p>'
                 + '    <small>Donec id elit non mi porta.</small>'
-                + '    </a >'
-                + ' </div >'
+                + '    </a>'
+                + ' </div>'
             $('.row').append(html);
         }
         
