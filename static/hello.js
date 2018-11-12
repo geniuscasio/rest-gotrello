75f713e37afea84b55fa25e7e1d7086eb0ceb53d
Valentine Shevchenko <geniuscasiohl820a@gmail.com>
2018-11-12 17:14:31 +0200

    +bootstrap, consuming rest test...

--- static/hello.js
+++ static/hello.js
@@ -1,9 +1,27 @@
 $(document).ready(function() {
     $.ajax({
-        url:"http://rest-trello-server.herokuapp.com/income/1"
+        url:"http://rest-trello-server.herokuapp.com/income/"
     }).then(function(data) {
-        $('.income-id').append(JSON.parse(data).id);
-        $('.income-hint').append(JSON.parse(data).hint);
-        $('.income-amount').append(JSON.parse(data).amount);
+        $('.row').append();
+        var jsonData = JSON.parse(data);
+        for (var i = 0; i < jsonData.length; i++) {
+            var income = jsonData[i];
+            console.log(income.hint);
+            var id = income.id;
+            var hint = income.hint;
+            var amount = income.amount;
+            var html = '<div class="col-sm - 3">'
+                + '< a href = "#" class="list-group-item list-group-item-action flex-column align-items-start active" >'
+                + '<div class="d-flex w-30 justify-content-between">'
+                + '        <h5 class="mb-1">' + amount + '$</h5>'
+                + '        <small>3 days ago</small>'
+                + '    </div>'
+                + '    <p class="mb-1">' + hint + '</p>'
+                + '    <small>Donec id elit non mi porta.</small>'
+                + '    </a >'
+                + ' </div >'
+            $('.row').append(html);
+        }
+        
     });
 })
