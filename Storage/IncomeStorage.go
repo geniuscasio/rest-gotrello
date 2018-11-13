d4f70a7647be5300008c356c088142221b9dd6c6
Valentine Shevchenko <geniuscasiohl820a@gmail.com>
2018-11-13 17:03:09 +0200

    New field Date:Time has been added to Income entitie

--- Storage/IncomeStorage.go
+++ Storage/IncomeStorage.go
@@ -8,17 +8,19 @@ import (
 
 var incomes []entities.Income
 
-//Save save entity to persistent
+//TODO: Storage will be working with PostgreSQL
+
+//Save save entity to storage
 func Save(i entities.Income) {
 	incomes = append(incomes, i)
 }
 
-//GetAll returns all entities in DB
+//GetAll returns all entities in storage
 func GetAll() []entities.Income {
 	return incomes
 }
 
-//GetByID returns
+//GetByID returns Income with given Id (always return something for now)
 func GetByID(id string) entities.Income {
 	var _id int64
 	if id == "" {
@@ -26,7 +28,7 @@ func GetByID(id string) entities.Income {
 	} else {
 		_id, _ = strconv.ParseInt(id, 0, 64)
 	}
-	income := entities.Income{ID: _id, Amount: 50.0, Hint: "GetById result", Tags: nil}
+	income := entities.Income{ID: _id, Amount: 50.0, Date: time.Now(), Hint: "GetById result", Tags: nil}
 	incomes = append(incomes, income)
 	return income
 }
--- entities/Income.go
+++ entities/Income.go
@@ -1,9 +1,12 @@
 package entities
 
+import "time"
+
 //Income struct represent Income object
 type Income struct {
 	ID     int64       `json:"id"`
 	Amount float32     `json:"amount"`
+	Date   Time		   `json:"date"`
 	Hint   string      `json:"hint,omitempty"`
 	Tags   []IncomeTag `json:"tags,omitempty"`
 }
