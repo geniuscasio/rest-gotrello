f415101f6fa33cf3e76621ff4b7099cdd8b42e2e
Valentine (laptop) <geniuscasiohl820a@gmail.com>
2018-11-15 21:21:39 +0200

    change static route to root (/)

--- main.go
+++ main.go
@@ -17,9 +17,9 @@ func logging(f http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		username, _ := r.Cookie("username")
 		fmt.Println(username)
-		
+
 		expiration := time.Now().Add(365 * 24 * time.Hour)
-    	cookie := http.Cookie{Name: "username", Value: time.Now().Format("2006-01-02 15:04:05"), Expires: expiration}
+		cookie := http.Cookie{Name: "username", Value: time.Now().Format("2006-01-02 15:04:05"), Expires: expiration}
 		http.SetCookie(w, &cookie)
 		fmt.Println("set cookie in logging")
 		f(w, r)
@@ -41,12 +41,17 @@ func main() {
 	flag.StringVar(&dir, "dir", "./static", "the directory to serve files from. Defaults to the current dir")
 	flag.Parse()
 	fmt.Println(dir)
-	// This will serve files under http://localhost:8000/static/<filename>
-	router.PathPrefix("/static").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir(dir))))
 
-	router.HandleFunc("/", logging(endpoints.Get)).Methods("GET")
-	router.HandleFunc("/income/{id}", logging(endpoints.Get)).Methods("GET")
-	router.HandleFunc("/income/", logging(endpoints.Create)).Methods("POST")
-	router.HandleFunc("/income/", logging(endpoints.Get)).Methods("GET")
+	// GETs
+	router.HandleFunc("/api/v1/", logging(endpoints.Get)).Methods("GET")
+	router.HandleFunc("/api/v1/income/", logging(endpoints.Get)).Methods("GET")
+	router.HandleFunc("/api/v1/income/{id}", logging(endpoints.Get)).Methods("GET")
+
+	// POSTs
+	router.HandleFunc("/api/v1/income/", logging(endpoints.Create)).Methods("POST")
+
+	// Static "/" must be last in code
+	router.PathPrefix("/").Handler(http.StripPrefix("/", http.FileServer(http.Dir(dir))))
+
 	log.Fatal(http.ListenAndServe(port, router))
 }
