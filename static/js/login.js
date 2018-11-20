0cbbb3330c3c3b2b25c396e472704cb3a1565373
Valentine (laptop) <geniuscasiohl820a@gmail.com>
2018-11-20 18:22:54 +0200

    login process changes

--- static/js/login.js
+++ static/js/login.js
@@ -1,11 +1,37 @@
-function check(form)/*function to check userid & password*/ {
-    /*the following code checkes whether the entered userid and password are matching*/
-    if (form.userid.value == "myuserid" && form.pswrd.value == "mypswrd") {
-        alert("ok")/*opens the target page while Id & password matches*/
-        Console.log("ok")
+String.prototype.hashCode = function() {
+    var hash = 0, i, chr;
+    if (this.length === 0) return hash;
+    for (i = 0; i < this.length; i++) {
+        chr   = this.charCodeAt(i);
+        hash  = ((hash << 5) - hash) + chr;
+        hash |= 0; // Convert to 32bit integer
     }
-    else {
-        alert("Error Password or Username")/*displays error message*/
-        Console.log("err")
-    }
+    return hash;
+  };
+
+function check(form) {
+    userName = form.userName.value;
+    userHash = form.password.value.hashCode();
+    method = "post";
+
+    var form = document.createElement("form");
+    form.setAttribute("method", method);
+    form.setAttribute("action", "api/v1/login/");
+
+    var fieldUserName = document.createElement("input");
+    fieldUserName.setAttribute("type", "hidden");
+    fieldUserName.setAttribute("name", "userName");
+    fieldUserName.setAttribute("value", userName);
+    form.appendChild(fieldUserName);
+
+    var fieldUserHash = document.createElement("input");
+    fieldUserHash.setAttribute("type", "hidden");
+    fieldUserHash.setAttribute("name", "userHash");
+    fieldUserHash.setAttribute("value", userHash);
+    form.appendChild(fieldUserHash);
+
+    console.log(userName+' '+userHash);
+
+    document.body.appendChild(form);
+    form.submit();
 }
