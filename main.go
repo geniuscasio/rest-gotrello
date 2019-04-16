9f886a8678f680541e7e9163142c6aaabb039cca
Valentine Shevchenko <geniuscasiohl820a@gmail.com>
2019-04-16 17:36:18 +0300

    Add 2 check func to ensure inMsg is correct

--- main.go
+++ main.go
@@ -7,19 +7,50 @@ import (
 	"net/http"
 	"os"
 	"encoding/json"
+	"unicode/utf8"
+	"regexp"
+	"strconv"
+	"strings"
+	"errors"
+	
 	ends "github.com/geniuscasio/rest-gotrello/endpoints"
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 )
 
 type inMsg struct {
-	Phone string `json:"phone"`
+	Messages []struct {
+		Phone   int64  `json:"phone_number"`
+		Extraid string `json:"extra_id"`
+		Text    string `json:"text,omitempty"`
+	} `json:"messages"`
+	CallbackURL    string   `json:"callback_url"`
+	StartTime      string   `json:"start_time"`
+	Tag            string   `json:"tag"`
+	Channels       []string `json:"channels"`
+	ChannelOptions struct {
+		Sms struct {
+			AlphaName string `json:"alpha_name,omitempty"`
+			TTL       int64  `json:"ttl,omitempty"`
+		} `json:"sms,omitempty"`
+		Viber struct {
+			TTL     int64  `json:"ttl"`
+			Img     string `json:"img"`
+			Caption string `json:"caption"`
+			Action  string `json:"action"`
+		} `json:"viber"`
+	} `json:"channel_options"`
 }
 
 type outMsg struct {
-	Phone  string `json:"phone"`
+	Phone  string `json:"phone,omitempty"`
 	Status string `json:"status"`
 }
+
+type IErrorCheck func(inMsg) error
+
+const LATIN_MAX_SIZE = 765
+const CYRILIC_MAX_SIZE = 365
 
 func main() {
 	router := mux.NewRouter()
@@ -53,19 +84,76 @@ func main() {
 }
 
 func sayHello(w http.ResponseWriter, r *http.Request) {
-	var in []inMsg
+	var in inMsg
 	decoder := json.NewDecoder(r.Body)
 	err := decoder.Decode(&in)
 	if err != nil {
+		fmt.Println(err)
 		return
 	}
-	var out []outMsg
-	for k, i := range in {
-		fmt.Println(i, k)
-		out = append(out, outMsg{Phone: i.Phone, Status: "ok"})
+	errorExists := false
+	checkError := func(f IErrorCheck) {
+		if errorExists == true {
+			fmt.Println("Another one bite to dust")
+			return
+		}
+		err := f(in)
+		if err != nil {
+			errorExists = true
+			fmt.Println("Error", err)
+			out := outMsg{Status: "Error " + err.Error()}
+			var buf []byte
+			buf, err = json.Marshal(out)
+			_, err = w.Write(buf)
+		}
 	}
-	// fmt.Println(in.Phone)
-	var buf []byte
-	buf, err = json.Marshal(out)
-	_, err = w.Write(buf)
+	checkError(checkNumber)
+	checkError(checkText)
+}
+
+func checkText(i inMsg) error {
+	var isViber bool
+	var isSms bool
+	var maximumSize int = 1000
+	for _, b := range i.Channels {
+		if b == "sms" {
+			isSms = true
+		}
+		if b == "viber" {
+			isViber = true
+		}
+	}
+	for _, msg := range i.Messages {
+		if isSms && isViber {
+			if isCyrilic(msg.Text) {
+				maximumSize = CYRILIC_MAX_SIZE
+			} else {
+				maximumSize = LATIN_MAX_SIZE
+			}
+		} else if isViber {
+			maximumSize = 1000
+		}
+	}
+	for _, msg := range i.Messages {
+		if utf8.RuneCountInString(msg.Text) > maximumSize || utf8.RuneCountInString(msg.Text) == 0 {
+			return fmt.Errorf("checkText: msg=%s minSize=1 maxSize=%d actualSize=%d", msg.Text, maximumSize, utf8.RuneCountInString(msg.Text))
+		}
+	}
+	return nil
+}
+
+func isCyrilic(s string) bool {
+	var re = regexp.MustCompile(`(?m)\p{Cyrillic}`)
+	_isCyrilic := re.Match([]byte(s))
+	return _isCyrilic
+}
+
+func checkNumber(i inMsg) error {
+	for _, i := range i.Messages {
+		n := strconv.Itoa(int(i.Phone))
+		if len(n) != 12 || !strings.HasPrefix(n, "380") {
+			return errors.New("checkNumber: invalid phone number")
+		}
+	}
+	return nil
 }
