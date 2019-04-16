ad42e85513078b9e0c070eb6b0a21865ce60d15e
Valentine Shevchenko <geniuscasiohl820a@gmail.com>
2019-04-16 14:15:58 +0300

    Update main.go

--- main.go
+++ main.go
@@ -6,11 +6,20 @@ import (
 	"log"
 	"net/http"
 	"os"
-
+	"encoding/json"
 	ends "github.com/geniuscasio/rest-gotrello/endpoints"
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 )
+
+type inMsg struct {
+	Phone string `json:"phone"`
+}
+
+type outMsg struct {
+	Phone  string `json:"phone"`
+	Status string `json:"status"`
+}
 
 func main() {
 	router := mux.NewRouter()
@@ -34,9 +43,29 @@ func main() {
 
 	// POSTs
 	router.HandleFunc("/api/v1/income/", ends.SecureEndpoint(ends.Create)).Methods("POST")
+	
+	router.HandleFunc("/test", sayHello).Methods("GET")
 
 	// Static "/" must be last in code
 	router.PathPrefix("/").Handler(http.StripPrefix("/", http.FileServer(http.Dir(dir))))
 
 	log.Fatal(http.ListenAndServe(port, router))
+}
+
+func sayHello(w http.ResponseWriter, r *http.Request) {
+	var in []inMsg
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&in)
+	if err != nil {
+		return
+	}
+	var out []outMsg
+	for k, i := range in {
+		fmt.Println(i, k)
+		out = append(out, outMsg{Phone: i.Phone, Status: "ok"})
+	}
+	// fmt.Println(in.Phone)
+	var buf []byte
+	buf, err = json.Marshal(out)
+	_, err = w.Write(buf)
 }
