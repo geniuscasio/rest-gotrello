1921d8c00c393d81e9bd1c7737fa62cbaa800da7
geniuscasiohl820a <geniuscasiohl820@gmail.com>
2019-05-07 14:34:44 +0300

    working with db

---
+++ db/db.go
@@ -0,0 +1,53 @@
+package dbHelper
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+)
+
+var db *sql.DB
+
+func getDB() *sql.DB {
+	if db == nil {
+		fmt.Printf("connect to db")
+		dbinfo := os.Getenv("DB_INFO")
+		dbinfo = "postgres://nhshglygkfiair:5d42deb354a442697dea6593f1f2bb6e0e869bda02adaad22d3cdcbd321671e1@ec2-23-21-122-141.compute-1.amazonaws.com:5432/d2dorvldn0g3nl"
+		var err error
+		db, err = sql.Open("postgres", dbinfo)
+		if err != nil {
+			fmt.Printf(err.Error())
+			return nil
+		}
+	}
+
+	return db
+}
+
+func InitDB() {
+	c := getDB()
+	_, r := c.Exec(`
+	CREATE TABLE MY_USERS(
+		user_id serial PRIMARY KEY, 
+		username VARCHAR (100) UNIQUE NOT NULL, 
+		password VARCHAR (100) NOT NULL
+	)`)
+	c.Exec(`
+		INSERT INTO MY_USERS(username, password) VALUES ("admin", "-995833633")
+	`)
+	if r != nil {
+		fmt.Println(r.Error())
+	}
+}
+
+func getUser(name string) (pass string) {
+	stmt, err := getDB().Prepare("SELECT password FROM MY_USERS WHERE username=$1")
+	if err != nil {
+		fmt.Errorf(err.Error())
+	}
+	res, _ := stmt.Query(name)
+	for res.Next() {
+		err = res.Scan(&pass)
+	}
+	return ""
+}
--- main.go
+++ main.go
@@ -1,51 +1,21 @@
 package main
 
 import (
-	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
+	dbHelper "github.com/geniuscasio/rest-gotrello/db"
 	ends "github.com/geniuscasio/rest-gotrello/endpoints"
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 )
 
-var db *sql.DB
-
-func getDB() *sql.DB {
-	if db == nil {
-		fmt.Printf("connect to db")
-		dbinfo := os.Getenv("DB_INFO")
-		dbinfo = "postgres://nhshglygkfiair:5d42deb354a442697dea6593f1f2bb6e0e869bda02adaad22d3cdcbd321671e1@ec2-23-21-122-141.compute-1.amazonaws.com:5432/d2dorvldn0g3nl"
-		var err error
-		db, err = sql.Open("postgres", dbinfo)
-		if err != nil {
-			fmt.Printf(err.Error())
-			return nil
-		}
-	}
-	return db
-}
-
-func initDB() {
-	c := getDB()
-	_, r := c.Exec(`
-	CREATE TABLE MY_USERS(
-		user_id serial PRIMARY KEY, 
-		username VARCHAR (100) UNIQUE NOT NULL, 
-		password VARCHAR (100) NOT NULL
-	)`)
-	if r != nil {
-		fmt.Println(r.Error())
-	}
-}
-
 func main() {
 	router := mux.NewRouter()
-	initDB()
+	dbHelper.InitDB()
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8005"
