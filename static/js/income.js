55f726321bf46f4a0ff4a1a395064c041fac1422
Shevchenko Valentine <geniuscasiohl820a@gmail.com>
2019-05-21 17:56:09 +0300

    js fix

--- static/js/income.js
+++ static/js/income.js
@@ -128,7 +128,7 @@ function deleteIncome(id){
     var isApproved = confirm("Вы - администратор?");
     if(isApproved) {
         var xhr = new XMLHttpRequest();
-        xhr.open("DELETE", "api/v1/incomeincomeDelete/", true);
+        xhr.open("DELETE", "api/v1/incomeDelete/", true);
         xhr.setRequestHeader('Content-Type', 'application/json');
         xhr.send(JSON.stringify({
             id: parseInt(id, 10)
